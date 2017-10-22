cdf0e47592fe2557fe49353d6b640119cbe34342
fredz0003 <ic3balandran@yahoo.com>
2017-10-21 23:48:32 -0500

    Added Best Travel files.

---
+++ Code-Wars-Python-2/Source/BestTravel.py
@@ -0,0 +1,4 @@
+
+
+def choose_best_sum(t, k, ls):
+    print("Test FAB")
---
+++ Code-Wars-Python-2/Tests/BestTravelTest.py
@@ -0,0 +1,6 @@
+import unittest
+import BestTravel
+
+class Choose_best_sum_test(unittest.TestCase):
+    def test(self):
+        BestTravel.choose_best_sum(1,2,3)
